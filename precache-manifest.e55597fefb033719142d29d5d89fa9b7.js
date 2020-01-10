289369529aedde4684af74e5632036a8df0cadf6
Jo <jo3cyc@gmail.com>
2020-01-10 18:53:53 +1100

    Updates

---
+++ precache-manifest.e55597fefb033719142d29d5d89fa9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c6f7d68722dc41c9c662e06bafebef",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7b4a3d8357785aff119c",
+    "url": "/weather-app/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "b09656fe535b891a7fcf",
+    "url": "/weather-app/static/js/2.bf543d58.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.bf543d58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b4a3d8357785aff119c",
+    "url": "/weather-app/static/js/main.1e6cc0d7.chunk.js"
+  },
+  {
+    "revision": "f6e79f44ce08fb03d80e",
+    "url": "/weather-app/static/js/runtime-main.bf761b10.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/weather-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1e2ed0028c8bbd29dcd4c43e744070b.js"
+  "/weather-app/precache-manifest.e55597fefb033719142d29d5d89fa9b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
